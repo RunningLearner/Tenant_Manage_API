b8340b7bd7fb0f0ea76adc3370ec15d57df0b0ea
조진식 <jscho@sds.co.kr>
2023-04-17 07:36:54 +0000

    Merged PR 3161: 2번째 과제 - 자동차 경주 게임 만들기 준비
    2번째 과제 - 자동차 경주 게임 만들기 프로젝트 초기화
    
    Related work items: #21095

---
+++ Challenge02-CarRacing/Car.cs
@@ -0,0 +1,14 @@
+namespace Challenge02_CarRacing;
+
+public class Car
+{
+    private readonly string name;
+    private int position = 0;
+
+    public Car(string name)
+    {
+        this.name = name;
+    }
+
+    // 이 줄 아래부터 추가 기능 구현
+}
