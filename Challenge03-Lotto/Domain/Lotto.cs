ea76e407dbe472d3cb8f555673accc8c95667ee4
조진식 <jscho@sds.co.kr>
2023-05-30 00:51:21 +0000

    Merged PR 3229: 임시 커밋
    과제물을 내어주기 위한 임시 커밋
    
    Related work items: #21489

---
+++ Challenge03-Lotto/Domain/Lotto.cs
@@ -0,0 +1,13 @@
+﻿namespace Challenge03_Lotto.Domain;
+
+public class Lotto
+{
+    private readonly List<int> _numbers;
+
+    public Lotto(List<int> numbers)
+    {
+        _numbers = numbers;
+    }
+
+    // 이 아래로 추가 기능 구현
+}
---
+++ Challenge03-Lotto/Domain/WinningLotto.cs
@@ -0,0 +1,15 @@
+﻿namespace Challenge03_Lotto.Domain;
+
+public class WinningLotto
+{
+    private readonly Lotto _lotto;
+    private readonly int _bonusNumber;
+
+    public WinningLotto(Lotto lotto, int bonusNumber)
+    {
+        _lotto = lotto;
+        _bonusNumber = bonusNumber;
+    }
+
+    // 이 아래로 추가 기능 구현
+}
