bdd5632b160a701e701d99d672029e4d7785d8ae
남승인 <sinam@sds.co.kr>
2023-06-23 14:46:54 +0900

    feat: 호스트 서비스 등록

--- Challenge04-TenantManagementApi/Startup.cs
+++ Challenge04-TenantManagementApi/Startup.cs
@@ -94,6 +94,9 @@ public class Startup
         var graphServiceClient = new GraphServiceClient(clientCertCredential);
         services.AddSingleton(graphServiceClient);
 
+        // 주기적으로 작동하는 data fetcher 등록
+        services.AddHostedService<TimedDataFetchingService>();
+
         services.AddScoped<UserService>();
         services.AddScoped<GroupService>();
     }
