916b4e4973eab65be4224a2684d6547b4a92d802
남승인 <sinam@sds.co.kr>
2023-06-23 13:38:26 +0900

    fix: init을 set으로 변경

--- Challenge04-TenantManagementApi/Models/Group.cs
+++ Challenge04-TenantManagementApi/Models/Group.cs
@@ -2,11 +2,11 @@ namespace Challenge04_TenantManagementApi.Models;
 
 public class Group
 {
-    public required string Id { get; init; }
+    public required string Id { get; set; }
 
-    public string? DisplayName { get; init; }
+    public string? DisplayName { get; set; }
 
-    public string? Description { get; init; }
+    public string? Description { get; set; }
 
-    public string? MailNickname { get; init; }
+    public string? MailNickname { get; set; }
 }
--- Challenge04-TenantManagementApi/Models/User.cs
+++ Challenge04-TenantManagementApi/Models/User.cs
@@ -2,11 +2,11 @@ namespace Challenge04_TenantManagementApi.Models;
 
 public class User
 {
-    public required string Id { get; init; }
+    public required string Id { get; set; }
 
-    public string? DisplayName { get; init; }
+    public string? DisplayName { get; set; }
 
-    public string? UserPrincipalName { get; init; }
+    public string? UserPrincipalName { get; set; }
 
-    public string? MailNickname { get; init; }
+    public string? MailNickname { get; set; }
 }
