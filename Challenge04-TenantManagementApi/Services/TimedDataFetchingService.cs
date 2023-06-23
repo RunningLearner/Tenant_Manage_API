3e39803ee5c191d40f96d5e2ec7462af0cfd17bb
남승인 <sinam@sds.co.kr>
2023-06-23 14:31:05 +0900

    feat: 데이터 fetching 로직 추가

--- Challenge04-TenantManagementApi/Services/TimedDataFetchingService.cs
+++ Challenge04-TenantManagementApi/Services/TimedDataFetchingService.cs
@@ -1,4 +1,10 @@
+using Challenge04_TenantManagementApi.Data;
 using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using DbUser = Challenge04_TenantManagementApi.Models.User;
+using GraphUser = Microsoft.Graph.Models.User;
+using DbGroup = Challenge04_TenantManagementApi.Models.Group;
+using GraphGroup = Microsoft.Graph.Models.Group;
 
 namespace Challenge04_TenantManagementApi.Services;
 
@@ -7,10 +13,12 @@ public sealed class TimedDataFetchingService : BackgroundService
     private readonly ILogger<TimedDataFetchingService> _logger;
     private readonly GraphServiceClient _graphClient;
 
-    public TimedDataFetchingService(ILogger<TimedDataFetchingService> logger, GraphServiceClient graphClient)
+    private readonly GraphDbContext _dbContext;
+    public TimedDataFetchingService(ILogger<TimedDataFetchingService> logger, GraphServiceClient graphClient, GraphDbContext dbContext)
     {
         _logger = logger;
         _graphClient = graphClient;
+        _dbContext = dbContext;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -21,13 +29,90 @@ public sealed class TimedDataFetchingService : BackgroundService
         {
             _logger.LogInformation("Timed Data Fetching Service is working.");
 
-            var users = await _graphClient.Users.GetAsync(cancellationToken: stoppingToken);
-
-            // 로직 처리 부분입니다.
+            await FetchUserData();
+            await FetchGroupData();
 
             await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
         }
 
         _logger.LogInformation("Timed Data Fetching Service is stopping.");
+    }
+
+    private async Task FetchUserData()
+    {
+        var usersResponse = await _graphClient.Users.GetAsync(requestConfiguration =>
+         {
+             requestConfiguration.QueryParameters.Select = new[] { "id", "displayName", "userPrincipalName", "mailNickname" };
+         }) ?? throw new ArgumentNullException("usersResponse", "No users were found.");
+
+        var pageIterator = PageIterator<GraphUser, UserCollectionResponse>
+                    .CreatePageIterator(_graphClient, usersResponse, async (user) =>
+                    {
+                        var dbUser = await _dbContext.Users.FindAsync(user.Id);
+
+                        if (dbUser == null)
+                        {
+                            _dbContext.Users.Add(new DbUser
+                            {
+                                Id = user.Id!,
+                                DisplayName = user.DisplayName,
+                                UserPrincipalName = user.UserPrincipalName,
+                                MailNickname = user.MailNickname
+                            });
+                        }
+                        else
+                        {
+                            _dbContext.Users.Update(new DbUser
+                            {
+                                Id = user.Id!,
+                                DisplayName = user.DisplayName,
+                                UserPrincipalName = user.UserPrincipalName,
+                                MailNickname = user.MailNickname
+                            });
+                        }
+
+                        return true;
+                    });
+
+        await pageIterator.IterateAsync();
+    }
+
+    private async Task FetchGroupData()
+    {
+        var groupsResponse = await _graphClient.Groups.GetAsync(requestConfiguration =>
+         {
+             requestConfiguration.QueryParameters.Select = new[] { "id", "displayName", "description", "mailNickname" };
+         }) ?? throw new ArgumentNullException("groupsResponse", "No users were found.");
+
+        var pageIterator = PageIterator<GraphGroup, GroupCollectionResponse>
+                    .CreatePageIterator(_graphClient, groupsResponse, async (group) =>
+                    {
+                        var dbGroup = await _dbContext.Groups.FindAsync(group.Id);
+
+                        if (dbGroup == null)
+                        {
+                            _dbContext.Groups.Add(new DbGroup
+                            {
+                                Id = group.Id!,
+                                DisplayName = group.DisplayName,
+                                Description = group.Description,
+                                MailNickname = group.MailNickname
+                            });
+                        }
+                        else
+                        {
+                            _dbContext.Groups.Update(new DbGroup
+                            {
+                                Id = group.Id!,
+                                DisplayName = group.DisplayName,
+                                Description = group.Description,
+                                MailNickname = group.MailNickname
+                            });
+                        }
+
+                        return true;
+                    });
+
+        await pageIterator.IterateAsync();
     }
 }
