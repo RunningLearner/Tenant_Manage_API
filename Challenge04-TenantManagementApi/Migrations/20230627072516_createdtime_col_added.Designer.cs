0084d3d7c21e524e0ab530c6b66679ee108d3bff
남승인 <sinam@sds.co.kr>
2023-06-27 16:58:56 +0900

    feat: 새로운 스키마 추가에 의한 마이그레이션 추가

--- Challenge04-TenantManagementApi/Migrations/20230627072516_createdtime_col_added.Designer.cs
+++ Challenge04-TenantManagementApi/Migrations/20230627072516_createdtime_col_added.Designer.cs
@@ -2,6 +2,7 @@
 using Challenge04_TenantManagementApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Challenge04_TenantManagementApi.Migrations
 {
     [DbContext(typeof(GraphDbContext))]
-    partial class GraphDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230627072516_createdtime_col_added")]
+    partial class createdtime_col_added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.8");
---
+++ Challenge04-TenantManagementApi/Migrations/20230627072516_createdtime_col_added.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Challenge04_TenantManagementApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class createdtime_col_added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedDateTime",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedDateTime",
+                table: "Groups",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDateTime",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDateTime",
+                table: "Groups");
+        }
+    }
+}
