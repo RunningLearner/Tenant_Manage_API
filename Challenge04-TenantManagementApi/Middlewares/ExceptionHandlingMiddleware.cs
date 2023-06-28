caea35b4d4388e20e1f269384863134007a9723e
남승인 <sinam@sds.co.kr>
2023-06-28 10:13:46 +0900

    refactor: problem detail 형식으로 예외를 처리하도록 수정

--- Challenge04-TenantManagementApi/Middlewares/ExceptionHandlingMiddleware.cs
+++ Challenge04-TenantManagementApi/Middlewares/ExceptionHandlingMiddleware.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Graph.Models.ODataErrors;
 
 namespace Challenge04_TenantManagementApi.Middlewares;
@@ -34,28 +35,41 @@ public class ExceptionHandlingMiddleware : IMiddleware
     {
         context.Response.ContentType = "application/json";
         var response = context.Response;
-        string errorResponse = exception.Message;
+        var problemDetails = new ProblemDetails
+        {
+            Instance = context.Request?.Path,
+            Title = exception.GetType().Name,
+            Detail = exception.Message
+        };
 
         switch (exception)
         {
             case ODataError ex:
                 response.StatusCode = ex.ResponseStatusCode;
+                problemDetails.Status = ex.ResponseStatusCode;
+                problemDetails.Type = $"https://httpstatuses.com/{ex.ResponseStatusCode}";
                 break;
             case KeyNotFoundException:
             case ArgumentNullException:
                 response.StatusCode = (int)HttpStatusCode.NotFound;
+                problemDetails.Status = (int)HttpStatusCode.NotFound;
+                problemDetails.Type = "https://httpstatuses.com/404";
                 break;
             case ArgumentException:
                 response.StatusCode = (int)HttpStatusCode.BadRequest;
+                problemDetails.Status = (int)HttpStatusCode.BadRequest;
+                problemDetails.Type = "https://httpstatuses.com/400";
                 break;
             default:
                 _logger.LogError(exception, "서버 에러 발생");
                 response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                errorResponse = "Internal server error!";
+                problemDetails.Status = (int)HttpStatusCode.InternalServerError;
+                problemDetails.Type = "https://httpstatuses.com/500";
+                problemDetails.Detail = "Internal server error!";
                 break;
         }
 
-        var result = JsonSerializer.Serialize(new { message = errorResponse });
+        var result = JsonSerializer.Serialize(problemDetails);
         await context.Response.WriteAsync(result);
     }
 }
